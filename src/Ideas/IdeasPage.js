71b82292be1b6a70ee1080ad13a24f5895ebc0dc
Luke Colombo <lukedwardc@gmail.com>
2025-06-07 17:19:28 -0400

    updated ideas page

--- src/Ideas/IdeasPage.js
+++ src/Ideas/IdeasPage.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import "./Ideas.css"
 
 export default function IdeasPage() {
     return (
-        <div>
+        <div className={"ideas-cont"}>
             <h1>Bracket Maker Help</h1>
-            <h2>Overview</h2>
-            <p>A Voting Bracket is my name for a game where one or multiple people simulate a March
-                Madness style tournament for things other than sports teams. I've seen brackets for
-                favorite rap songs, fast food places, movies, basically anything you can think of.
+            <h2 className={"ideas-header"}>Overview</h2>
+            <p className={"ideas-text"}>A Voting Bracket is my name for a game where one or multiple
+                people simulate a March
+                Madness style tournament for things other than sports teams. Like tier lists, you
+                can make a bracket of anything you can think of - favorite songs, movies, fast food
+                places, etc.
                 They're usually single-elimination tournament style, so the highest seed plays the
-                lowest seed, and the winners advance. At the end,
-                the winner can be crowned 'The Group's Favorite Movie', for example. However, most
+                lowest seed, and the winners advance.</p>
+            <p className={"ideas-text"}>At the end,
+                the winner can be crowned 'The Group's Favorite Movie', for example. But don't worry
+                too much about the ultimate winner - most
                 of the fun is seeing what makes it far and getting unconventional matchups that you
-                might not have thought of yourself. Don't worry too much about the ultimate
-                winner—journey before destination, as some would say.</p>
-            <h2>Seeding</h2>
-            <p>Seeding can be done a number of ways. I
+                might not have thought of yourself. </p>
+            <h2 className={"ideas-header"}>Seeding</h2>
+            <p className={"ideas-text"}>Seeding can be done a number of ways. I
                 recommend random, or using some external metric that you can easily look up—for
-                example, number of streams on Spotify, a random magazine's ranking, etc.</p>
+                example, number of streams on Spotify, a ranking from a magazine or blog, etc.</p>
+            <h2 className={"ideas-header"}>Ideas</h2>
+            <p className={"ideas-text"}>Need ideas? Below is a short list of some categories to help you brainstorm.</p>
+            <ul>
+                <li>Favorite animated movies</li>
+                <li>Favorite action movies</li>
+                <li>Best songs of all time</li>
+                <li>Worst songs of all time</li>
+                <li>Sports team logos</li>
+                <li>Favorite animals</li>
+                <li>Which animals would win in a fight</li>
+                <li>Album covers</li>
+                <li>TV show intros</li>
+            </ul>
         </div>
     );
 }
